6ca5312eff60bfd8821af45255640264bfdc87e4
Andrew Wang <stonefish1089@gmail.com>
2019-12-18 03:30:01 -0500

    Reset indexes after data preprocessing, added functions to a .py file for ease of calling, small script in Testing_Cleaned_Data_Preprocessing.ipnyb for testing/calling the data processing functions.

---
+++ Cleaned_Data_Preprocessing.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import copy
+
+
+def train_test_split(X, y, train_ratio=0.8):
+    random_mask = np.random.permutation(len(y))
+    X_rand = X.iloc[random_mask]
+    y_rand = y.iloc[random_mask]
+    
+    split_ind = int(train_ratio * len(y))
+    X_train = X_rand.iloc[:split_ind].reset_index()
+    y_train = y_rand.iloc[:split_ind].reset_index()
+    
+    X_test = X_rand.iloc[split_ind:].reset_index()
+    y_test = y_rand.iloc[split_ind:].reset_index()
+    
+    return X_train, y_train, X_test, y_test
+    
+
+def balance_data_by_label(X, y, target_1_0_ratio):
+    mask_1 = pd.Series([y['y'] == 1][0])
+    X_1 = X.loc[mask_1]
+    y_1 = y.loc[mask_1] # Unnecessary: We know these are all 1's
+    num_1 = sum(mask_1)
+    num_0 = len(y) - num_1
+    
+    curr_1_0_ratio = num_1 / num_0
+    # Float value
+    num_dupes = float(target_1_0_ratio) / curr_1_0_ratio - 1
+    
+    while num_dupes > 1:
+        
+        # @TODO: append X_1 to X, and 1's to y
+#         X = np.vstack((X, copy.deepcopy(X_1)))
+#         y = np.vstack((y, np.ones((num_1, 1))))
+        X = X.append(copy.deepcopy(X_1))
+        y = y.append(copy.deepcopy(y_1))
+        num_dupes -= 1
+    
+    #Adding on additional individual duplicates chosen at random from dupe_1 to meet target_1_total_ratio:
+    if num_dupes > 0:
+        # @TODO: append X_to_append to X, and 1's to y
+#         X_to_append = copy.deepcopy(X_1[:int(num_dupes * num_1)])
+#         y_to_append = np.ones((int(num_dupes * num_1), 1))
+#         X = np.vstack((X, X_to_append))
+#         y = np.vstack((y, y_to_append))
+        X = X.append(copy.deepcopy(X_1.loc[:int(num_dupes * num_1)]))
+        y = y.append(copy.deepcopy(y_1.loc[:int(num_dupes * num_1)]))
+        
+    return X.reset_index(), y.reset_index()
